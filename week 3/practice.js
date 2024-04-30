73605002b3ff5b752e4e464ddc1da3c3f5140b0c
Sarthak231203 <>
2024-04-30 23:02:52 +0530

    week4 and chai or code files is been added

---
+++ week 3/practice.js
@@ -0,0 +1,14 @@
+let numberOfRequestsForUser = {
+    userId :"",
+    fName: "sarthak",
+    gender: "male"
+}
+
+
+
+numberOfRequestsForUser[userId] = 2;
+
+console.log(numberOfRequestsForUser[userId]);
+console.log(numberOfRequestsForUser);
+
+
