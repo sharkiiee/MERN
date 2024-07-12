774f28a5f66f80359f4693732e0407a98bd2d36c
Sarthak231203 <sarthak231203@gmail.com>
2024-07-12 21:30:13 +0530

    lazy loading issue is been solved

--- 0_Learn by program/17_ReactRouting/3.lazyLoading.jsx
+++ 0_Learn by program/17_ReactRouting/3.lazyLoading.jsx
@@ -1,32 +1,43 @@
-// Lazy Loading
+// Lazy loading 
+// Something asyncronouse component fetching is working that's why use suspence.
 
-// Still some issue please check the issue- the dashboard files are coming with some extra files.
-import { BrowserRouter, Routes, Route} from 'react-router-dom'
-import { lazy, Suspense } from 'react';
+// Always make exported files default.
+
+// Write a program that shows lazy loading of the page.
+
+import { BrowserRouter, Routes, Route, useNavigate} from 'react-router-dom'
+import { Suspense, lazy } from 'react';
 const Landing = lazy(() => import('./components/Landing'))
 const Dashboard = lazy(() => import('./components/Dashboard'))
 
 function App()
 {
+
     return (
         <div>
-            <div>
-                <button onClick={()=> {
-                    window.location.href = "/";
-                }}>Landing Button</button>
-                <button onClick={()=> {
-                    window.location.href = "/dashboard";
-                }}>Dashboard Button</button>
-            </div>
         <BrowserRouter>
+        <Appbar/>
             <Routes>
-                <Route path='/dashboard' element={<Suspense fallback={"loading..."}> <Dashboard/> </Suspense>} />
-                <Route path='/' element={<Suspense fallback={"loading..."}> <Landing/> </Suspense>} />
+                <Route path='/dashboard' element={<Suspense fallback={"loading..."}><Dashboard/></Suspense>} />
+                <Route path='/' element={<Suspense fallback={"loading..."}><Landing/></Suspense>} />
             </Routes>        
         </BrowserRouter>
         </div>
         
     )
+}
+
+function Appbar()
+{
+    const navigate = useNavigate();
+    return <div>
+                <button onClick={()=> {
+                    navigate("/");  
+                }}>Landing Button</button>
+                <button onClick={()=> {
+                    navigate("/dashboard"); 
+                }}>Dashboard Button</button>
+        </div>
 }
 
 export default App;
